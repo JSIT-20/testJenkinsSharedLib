4e31a1aa3bdfbb7b9002ed57947faf0c702f75dc
JSIT-20 <jdeme051@uottawa.ca>
2023-10-04 16:43:41 -0400

    Testing using creds

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -2,5 +2,9 @@ def call(Map config = [:]) {
     def fileContents = libraryResource 'test.properties'
     props = readProperties(text:fileContents)
     env.var1 = props.var1
-    sh "echo $var1"
+    creds = props.creds
+    withCredentials([usernamePassword(credentialsId: creds, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]){
+        sh "echo $var1"
+        sh "echo $USERNAME"
+    }
 }
