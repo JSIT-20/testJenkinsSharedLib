07dfd4f78c52830d47fa0a92796bdf0b78bbf28d
JSIT-20 <jdeme051@uottawa.ca>
2023-10-04 16:49:54 -0400

    Use arguments

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,5 @@
-def call(Map config = [:]) {
-    def fileContents = libraryResource 'test.properties'
+def call(String team) {
+    def fileContents = libraryResource team + '.properties'
     props = readProperties(text:fileContents)
     env.var1 = props.var1
     creds = props.creds
