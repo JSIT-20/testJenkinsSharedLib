d4ba1852f8f613cf5948e70e4d5b2beeb6f2921f
JSIT-20 <jdeme051@uottawa.ca>
2023-10-04 16:26:20 -0400

    Testing reading from properties

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,6 @@
 def call(Map config = [:]) {
-    props = readProperties(file:'test.properties')
+    def fileContents = libraryResource 'test.properties'
+    props = readProperties(text:fileContents)
     env.var1 = props.var1
     sh "echo $var1"
 }
